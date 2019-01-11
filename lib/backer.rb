21b235b3fa592a8b0dce3514f134c4583d444e73
Talisa Erazo <talisaerazo@gmail.com>
2019-01-11 18:16:27 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -5,4 +5,9 @@ class Backer
     @name = name
     @backed_projects = []
   end
+
+  def back_project(project)
+    @backed_projects << project
+    project.add_backer(self) unless project.backers.include?(self)
+  end
 end
